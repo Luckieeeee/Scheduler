eec99da63648e7224a7f58a70412df6d147ef85f
Luckieeeee <jainahchen2016@gmail.com>
2023-06-06 10:36:08 -0400

    Profile Update implemented, post detail view in progress

--- add/urls.py
+++ add/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from .views import PostListView, PostDetailView
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='add-home'),
+    path('', PostListView.as_view(), name='add-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='add-detail'),
     path('about/', views.about, name='add-about'),
 ]
--- add/views.py
+++ add/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 
@@ -8,6 +9,15 @@ def  home(request):
         'posts': Post.objects.all()
     }
     return render(request, 'add/home.html', context)
+
+class PostListView(ListView):
+    model = Post # listview
+    template_name = 'add/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_created']
+
+class PostDetailView(DetailView):
+    model = Post # listview
 
 def about(request):
     return render(request, 'add/about.html', {'title': 'About' })
