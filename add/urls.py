052a7c2308fc26d1cf5aa66b964bf2997fb44b86
Luckieeeee <jainahchen2016@gmail.com>
2023-06-11 15:56:46 -0400

    implement user details, add functionality: create post, delete post, update post

--- add/urls.py
+++ add/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import PostListView, PostDetailView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 from . import views
 
 urlpatterns = [
     path('', PostListView.as_view(), name='add-home'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='add-detail'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='add-delete'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='add-update'),
+    path('post/new/', PostCreateView.as_view(), name='add-create'),
     path('about/', views.about, name='add-about'),
 ]
--- add/views.py
+++ add/views.py
@@ -1,6 +1,9 @@
+from typing import Optional
+from django.forms.models import BaseModelForm
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Post
 
 
@@ -12,12 +15,46 @@ def  home(request):
 
 class PostListView(ListView):
     model = Post # listview
-    template_name = 'add/home.html'
+    template_name = 'add/home.html' #<app>/<model>_<viewtype>.html
     context_object_name = 'posts'
     ordering = ['-date_created']
 
 class PostDetailView(DetailView):
     model = Post # listview
+
+class PostCreateView(LoginRequiredMixin,CreateView):
+    model = Post 
+    fields = ['title', 'content']
+
+    def form_valid(self, form: BaseModelForm) -> HttpResponse:
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
+    model = Post 
+    fields = ['title', 'content']
+
+    def form_valid(self, form: BaseModelForm) -> HttpResponse:
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self) -> bool:
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
+    model = Post 
+    success_url = '/'
+
+    def test_func(self) -> bool:
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+    
+
 
 def about(request):
     return render(request, 'add/about.html', {'title': 'About' })
